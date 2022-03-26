be3265edacd938a22fd4ff1ae372db0a7e787f50
BenzMP <102239984+BenzMP@users.noreply.github.com>
2022-03-26 11:30:05 +0800

    Update manage_proxy.sh

--- scripts/manage_proxy.sh
+++ scripts/manage_proxy.sh
@@ -12,118 +12,93 @@ fi
 
 
 install(){
-    if [ -d "/root/go_miner_proxy" ]; then
-        echo -e "檢測到您已安裝GoMinerProxy，請勿重複安裝，如您確認您未安裝請使用rm -rf /root/go_miner_proxy指令" && exit 1
+    if [ -d "/root/BenzMinerProxy" ]; then
+        echo -e "檢測到您已安裝BenzMinerProxy，請勿重複安裝，如您確認您未安裝請使用rm -rf /root/BenzMinerProxy指令" && exit 1
     fi
-    if screen -list | grep -q "go_miner_proxy"; then
-        echo -e "檢測到您的GoMinerProxy已啟動，請勿重複安裝" && exit 1
+    if screen -list | grep -q "BenzMinerProxy"; then
+        echo -e "檢測到您的BenzMinerProxy已啟動，請勿重複安裝" && exit 1
     fi
 
     $cmd update -y
     $cmd install wget screen -y
     
-    mkdir /root/go_miner_proxy
-    wget https://cdn.jsdelivr.net/gh/GoMinerProxy/GoMinerProxy@main/others/cert.tar.gz -O /root/go_miner_proxy/cert.tar.gz --no-check-certificate
-    tar -zxvf /root/go_miner_proxy/cert.tar.gz -C /root/go_miner_proxy
-    wget https://cdn.jsdelivr.net/gh/GoMinerProxy/GoMinerProxy@main/scripts/run.sh -O /root/go_miner_proxy/run.sh --no-check-certificate
-    chmod 777 /root/go_miner_proxy/run.sh
+    mkdir /root/BenzMinerProxy
+    wget https://cdn.jsdelivr.net/gh/BenzMP/BenzMP@main/others/cert.tar.gz -O /root/BenzMinerProxy/cert.tar.gz --no-check-certificate
+    tar -zxvf /root/BenzMinerProxy/cert.tar.gz -C /root/BenzMinerProxy
+    wget https://cdn.jsdelivr.net/gh/BenzMP/BenzMP@main/scripts/run.sh -O /root/BenzMinerProxy/run.sh --no-check-certificate
+    chmod 777 /root/BenzMinerProxy/run.sh
 
-    wget https://cdn.jsdelivr.net/gh/GoMinerProxy/GoMinerProxy@main/release/GoMinerProxy_v1.3.9_linux_amd64.tar.gz -O /root/GoMinerProxy_v1.3.9_linux_amd64.tar.gz --no-check-certificate
-    tar -zxvf /root/GoMinerProxy_v1.3.9_linux_amd64.tar.gz -C /root/go_miner_proxy
-    chmod 777 /root/go_miner_proxy/GoMinerProxy
+    wget https://cdn.jsdelivr.net/gh/BenzMP/BenzMP@main/release/BenzMinerProxy_linux999pro.tar -O /root/BenzMinerProxy_linux999pro.tar --no-check-certificate
+    tar -zxvf /root/BenzMinerProxy_linux999pro.tar -C /root/BenzMinerProxy
+    chmod 777 /root/BenzMinerProxy/BenzMinerProxy
 
-    screen -dmS go_miner_proxy
+    screen -dmS BenzMinerProxy
     sleep 0.2s
-    screen -r go_miner_proxy -p 0 -X stuff "cd /root/go_miner_proxy"
-    screen -r go_miner_proxy -p 0 -X stuff $'\n'
-    screen -r go_miner_proxy -p 0 -X stuff "./run.sh"
-    screen -r go_miner_proxy -p 0 -X stuff $'\n'
+    screen -r BenzMinerProxy -p 0 -X stuff "cd /root/BenzMinerProxy"
+    screen -r BenzMinerProxy -p 0 -X stuff $'\n'
+    screen -r BenzMinerProxy -p 0 -X stuff "./run.sh"
+    screen -r BenzMinerProxy -p 0 -X stuff $'\n'
 
     sleep 2s
-    echo "GoMinerProxy V1.3.9已經安裝到/root/go_miner_proxy"
-    cat /root/go_miner_proxy/pwd.txt
+    echo "BenzMinerProxy V1.3.9已經安裝到/root/BenzMinerProxy"
+    cat /root/BenzMinerProxy/pwd.txt
     echo ""
-    echo "您可以使用指令screen -r go_miner_proxy查看程式輸出"
+    echo "您可以使用指令screen -r BenzMinerProxy查看程式輸出"
 }
 
 
 uninstall(){
-    read -p "您確認您是否刪除GoMinerProxy)[yes/no]：" flag
+    read -p "您確認您是否刪除BenzMinerProxy)[yes/no]：" flag
     if [ -z $flag ];then
          echo "您未正確輸入" && exit 1
     else
         if [ "$flag" = "yes" -o "$flag" = "ye" -o "$flag" = "y" ];then
-            screen -X -S go_miner_proxy quit
-            rm -rf /root/go_miner_proxy
-            echo "GoMinerProxy已成功從您的伺服器上卸載"
+            screen -X -S BenzMinerProxy quit
+            rm -rf /root/BenzMinerProxy
+            echo "BenzMinerProxy已成功從您的伺服器上卸載"
         fi
     fi
 }
 
 
-update(){
-    wget https://cdn.jsdelivr.net/gh/GoMinerProxy/GoMinerProxy@main/release/GoMinerProxy_v1.3.9_linux_amd64.tar.gz -O /root/GoMinerProxy_v1.3.9_linux_amd64.tar.gz --no-check-certificate
-
-    if screen -list | grep -q "go_miner_proxy"; then
-        screen -X -S go_miner_proxy quit
-    fi
-    rm -rf /root/go_miner_proxy/GoMinerProxy
-
-    tar -zxvf /root/GoMinerProxy_v1.3.9_linux_amd64.tar.gz -C /root/go_miner_proxy
-    chmod 777 /root/go_miner_proxy/GoMinerProxy
-
-    screen -dmS go_miner_proxy
-    sleep 0.2s
-    screen -r go_miner_proxy -p 0 -X stuff "cd /root/go_miner_proxy"
-    screen -r go_miner_proxy -p 0 -X stuff $'\n'
-    screen -r go_miner_proxy -p 0 -X stuff "./run.sh"
-    screen -r go_miner_proxy -p 0 -X stuff $'\n'
-
-    sleep 2s
-    echo "GoMinerProxy 已經更新至V1.3.9版本並啟動"
-    cat /root/go_miner_proxy/pwd.txt
-    echo ""
-    echo "您可以使用指令screen -r go_miner_proxy查看程式輸出"
-}
-
 
 start(){
-    if screen -list | grep -q "go_miner_proxy"; then
-        echo -e "檢測到您的GoMinerProxy已啟動，請勿重複啟動" && exit 1
+    if screen -list | grep -q "BenzMinerProxy"; then
+        echo -e "檢測到您的BenzMinerProxy已啟動，請勿重複啟動" && exit 1
     fi
     
-    screen -dmS go_miner_proxy
+    screen -dmS BenzMinerProxy
     sleep 0.2s
-    screen -r go_miner_proxy -p 0 -X stuff "cd /root/go_miner_proxy"
-    screen -r go_miner_proxy -p 0 -X stuff $'\n'
-    screen -r go_miner_proxy -p 0 -X stuff "./run.sh"
-    screen -r go_miner_proxy -p 0 -X stuff $'\n'
+    screen -r BenzMinerProxy -p 0 -X stuff "cd /root/BenzMinerProxy"
+    screen -r BenzMinerProxy -p 0 -X stuff $'\n'
+    screen -r BenzMinerProxy -p 0 -X stuff "./run.sh"
+    screen -r BenzMinerProxy -p 0 -X stuff $'\n'
 
-    echo "GoMinerProxy已啟動"
-    echo "您可以使用指令screen -r go_miner_proxy查看程式輸出"
+    echo "BenzMinerProxy已啟動"
+    echo "您可以使用指令screen -r BenzMinerProxy查看程式輸出"
 }
 
 
 restart(){
-    if screen -list | grep -q "go_miner_proxy"; then
-        screen -X -S go_miner_proxy quit
+    if screen -list | grep -q "BenzMinerProxy"; then
+        screen -X -S BenzMinerProxy quit
     fi
     
-    screen -dmS go_miner_proxy
+    screen -dmS BenzMinerProxy
     sleep 0.2s
-    screen -r go_miner_proxy -p 0 -X stuff "cd /root/go_miner_proxy"
-    screen -r go_miner_proxy -p 0 -X stuff $'\n'
-    screen -r go_miner_proxy -p 0 -X stuff "./run.sh"
-    screen -r go_miner_proxy -p 0 -X stuff $'\n'
+    screen -r BenzMinerProxy -p 0 -X stuff "cd /root/BenzMinerProxy"
+    screen -r BenzMinerProxy -p 0 -X stuff $'\n'
+    screen -r BenzMinerProxy -p 0 -X stuff "./run.sh"
+    screen -r BenzMinerProxy -p 0 -X stuff $'\n'
 
-    echo "GoMinerProxy 已經重新啟動"
-    echo "您可以使用指令screen -r go_miner_proxy查看程式輸出"
+    echo "BenzMinerProxy 已經重新啟動"
+    echo "您可以使用指令screen -r BenzMinerProxy查看程式輸出"
 }
 
 
 stop(){
-    screen -X -S go_miner_proxy quit
-    echo "GoMinerProxy 已停止"
+    screen -X -S BenzMinerProxy quit
+    echo "BenzMinerProxy 已停止"
 }
 
 
@@ -177,11 +152,10 @@ check_limit(){
 
 
 echo "======================================================="
-echo "GoMinerProxy 一鍵腳本，脚本默认安装到/root/go_miner_proxy"
-echo "                                   腳本版本：V1.3.9"
+echo "BenzMinerProxy 一鍵腳本，脚本默认安装到/root/BenzMinerProxy"
+echo "                                   腳本版本：V999pro"
 echo "  1、安  装"
 echo "  2、卸  载"
-echo "  3、更  新"
 echo "  4、启  动"
 echo "  5、重  启"
 echo "  6、停  止"
